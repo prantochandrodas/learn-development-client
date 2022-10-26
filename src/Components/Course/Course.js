42384bfac117eb931ae923223c2879fcec7da179
Pranto das <prantochandrodas@gmail.com>
2022-10-27 00:31:57 +0600

    set premium button

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -2,20 +2,24 @@ import React from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import './Course.css'
 const Course = () => {
     const data = useLoaderData();
 
     return (
-        <div className='container my-5 w-50'>
+        <div className='container my-5 courseDetails'>
            <Card>
-            <Card.Img variant="top" src={data.picture} style={{height:"300px",width:'600px' ,margin:"20px auto"}} />
+            <Card.Img variant="top" src={data.picture}  style={{width:'75%',height:'400px',margin:'10px auto'}}/>
             <Card.Body>
               <Card.Title>{data.name}</Card.Title>
               <Card.Text>
                     {data?.about.length>250 ?
                        <>{data.about.slice(0,250)+'...'}<Link to={`/category/${data.id}`}>ReadMore</Link></> :
                         data.about}
+
+                      <h4 className='my-3'>Get the premium access for all the  facility</h4>
               </Card.Text>
+              <Button variant='success' className='w-100' style={{fontSize:'20px',padding:'10px 0'}}> Premium Access</Button>
             </Card.Body>
           </Card>
         </div>
