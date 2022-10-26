044bbe70f58c18f8541e51a33878492052f958a7
Pranto das <prantochandrodas@gmail.com>
2022-10-26 12:21:37 +0600

    create auth provider

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,59 +1,59 @@
-// import React from 'react';
-// import { useState } from 'react';
-// import { useContext } from 'react';
-// import Button from 'react-bootstrap/Button';
-// import Form from 'react-bootstrap/Form';
-// import { useNavigate } from 'react-router-dom';
-// import { AuthContext } from '../../contexts/AuthProvider';
-// const Login = () => {
-//     const [error,setError]=useState('');
+import React from 'react';
+import { useState } from 'react';
+import { useContext } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../contexts/AuthProvider';
+const Login = () => {
+    const [error,setError]=useState('');
    
-//     const navigate=useNavigate();
-//     const {signIn}=useContext(AuthContext);
-//     const handleSubmit = event => {
-//         event.preventDefault();
-//         const form = event.target;
-//         const email = form.email.value;
-//         const password = form.password.value;
+    const navigate=useNavigate();
+    const {signIn}=useContext(AuthContext);
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
 
-//         signIn(email, password)
-//             .then(result => {
-//                 const user = result.user;
-//                 console.log(user);
-//                 form.reset();
-//                 setError('');
-//                 navigate('/');
-//             })
-//             .catch(error => {
-//                 console.error(error)
-//                 setError(error.message);
-//             })
+        signIn(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+                form.reset();
+                setError('');
+                navigate('/');
+            })
+            .catch(error => {
+                console.error(error)
+                setError(error.message);
+            })
            
-//     }
-//     return (
-//         <div className='w-50 mx-auto'>
-//             <h1 className='mb-2'>Login</h1>
-//              <Form onSubmit={handleSubmit}>
-//             <Form.Group className="mb-3" controlId="formBasicEmail">
-//                 <Form.Label>Email address</Form.Label>
-//                 <Form.Control name="email" type="email" placeholder="Enter email" required />
+    }
+    return (
+        <div className='w-50 mx-auto'>
+            <h1 className='mb-2'>Login</h1>
+             <Form onSubmit={handleSubmit}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control name="email" type="email" placeholder="Enter email" required />
 
-//             </Form.Group>
+            </Form.Group>
 
-//             <Form.Group className="mb-3" controlId="formBasicPassword">
-//                 <Form.Label>Password</Form.Label>
-//                 <Form.Control name="password" type="password" placeholder="Password" required />
-//             </Form.Group>
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control name="password" type="password" placeholder="Password" required />
+            </Form.Group>
 
-//             <Button variant="primary" type="submit">
-//                 Login
-//             </Button>
-//             <Form.Text className="text-danger">
-//                 {error}
-//             </Form.Text>
-//         </Form>
-//         </div>
-//     );
-// };
+            <Button variant="primary" type="submit">
+                Login
+            </Button>
+            <Form.Text className="text-danger">
+                {error}
+            </Form.Text>
+        </Form>
+        </div>
+    );
+};
 
-// export default Login;
+export default Login;
