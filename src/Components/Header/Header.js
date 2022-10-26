85a356565afeb6bd414439d93d64913ed7862275
Pranto das <prantochandrodas@gmail.com>
2022-10-26 12:22:33 +0600

    set hader dark toggel button

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,68 +1,68 @@
-// import React, { useState } from 'react';
-// import { useContext } from 'react';
-// import { Button, Nav, Navbar } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// import logo from '../../Assets/img/siteLogo.webp';
-// import { AuthContext } from '../../contexts/AuthProvider';
-// import './Header.css';
-// const Header = () => {
-//     const [dark,setDark]=useState(false);
-//     const { user,logOut } = useContext(AuthContext);
-//     const handelLogout=()=>{
-//         logOut()
-//         .then(() => { })
-//             .catch(error => console.error(error))
-//     }
-//     const handelDarkState=()=>{
-//         if(dark){
-//             setDark(false);
-//         }else{
-//             setDark(true);
-//         }
-//     }
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../Assets/img/siteLogo.webp';
+import { AuthContext } from '../../contexts/AuthProvider';
+import './Header.css';
+const Header = () => {
+    const [dark,setDark]=useState(false);
+    const { user,logOut } = useContext(AuthContext);
+    const handelLogout=()=>{
+        logOut()
+        .then(() => { })
+            .catch(error => console.error(error))
+    }
+    const handelDarkState=()=>{
+        if(dark){
+            setDark(false);
+        }else{
+            setDark(true);
+        }
+    }
   
-//     return (
-//         <div>
-//             <Navbar collapseOnSelect className='mb-4 ' expand="lg" bg="light" variant="light">
+    return (
+        <div>
+            <Navbar collapseOnSelect className='mb-4 ' expand="lg" bg="light" variant="light">
 
-//                 <div className='container'>
-//                     <Navbar.Brand><Link to='/' className='ms-4'
-//                         style={{
-//                             textDecoration: 'none',
-//                             fontSize: '25px',
-//                             fontWeight: 'bolder',
-//                             color: 'black'
-//                         }}><img src={logo} alt="" style={{ height: '40px' }} /> Learn Development</Link></Navbar.Brand>
-//                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-//                     <Navbar.Collapse id="responsive-navbar-nav">
-//                         <Nav className="prantsLink ms-auto">
-//                             <Link to='/'>Home</Link>
-//                             <Link to='/courses'>Courses</Link>
-//                             <Link to='/courses'> FAQ</Link>
-//                             <Link to='/courses'>Blog</Link>
-//                             {user?.uid ?
-//                                 <Button onClick={handelLogout} className='ms-3'>Logout</Button>
-//                                 : <>
-//                                     <Link to='/login'>Loin</Link>
-//                                     <Link to='register'>Register</Link>
-//                                 </>
-//                             } 
-//                           {dark?
-//                             <Button onClick={handelDarkState} variant='secondary'>LightMode</Button>:  <Button onClick={handelDarkState} variant='dark'>Dark</Button>}
-//                         </Nav>
-//                         <Nav>
+                <div className='container'>
+                    <Navbar.Brand><Link to='/' className='ms-4'
+                        style={{
+                            textDecoration: 'none',
+                            fontSize: '25px',
+                            fontWeight: 'bolder',
+                            color: 'black'
+                        }}><img src={logo} alt="" style={{ height: '40px' }} /> Learn Development</Link></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="prantsLink ms-auto">
+                            <Link to='/'>Home</Link>
+                            <Link to='/courses'>Courses</Link>
+                            <Link to='/courses'> FAQ</Link>
+                            <Link to='/courses'>Blog</Link>
+                            {user?.uid ?
+                                <Button onClick={handelLogout} className='ms-3'>Logout</Button>
+                                : <>
+                                    <Link to='/login'>Loin</Link>
+                                    <Link to='register'>Register</Link>
+                                </>
+                            } 
+                          {dark?
+                            <Button onClick={handelDarkState} variant='secondary'>LightMode</Button>:  <Button onClick={handelDarkState} variant='dark'>Dark</Button>}
+                        </Nav>
+                        <Nav>
 
-//                             <Link to="/profile">
+                            <Link to="/profile">
 
-//                             </Link>
-//                         </Nav>
+                            </Link>
+                        </Nav>
 
-//                     </Navbar.Collapse>
+                    </Navbar.Collapse>
 
-//                 </div>
-//             </Navbar>
-//         </div>
-//     );
-// };
+                </div>
+            </Navbar>
+        </div>
+    );
+};
 
-// export default Header;
+export default Header;
